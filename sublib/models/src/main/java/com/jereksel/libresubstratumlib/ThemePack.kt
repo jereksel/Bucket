79ee7865c4092e4816657b4fb864a2c5fc8f981d
Andrzej Ressel <jereksel@gmail.com>
2017-07-14 18:34:06 +0200

    Fix tests

--- sublib/models/src/main/java/com/jereksel/libresubstratumlib/ThemePack.kt
+++ sublib/models/src/main/java/com/jereksel/libresubstratumlib/ThemePack.kt
@@ -1,3 +1,6 @@
 package com.jereksel.libresubstratumlib
 
-data class ThemePack(val themes: List<Theme>, val type3: Type3Data?)
+data class ThemePack(
+        val themes: List<Theme> = listOf(),
+        val type3: Type3Data? = null
+)
