c3afd91d97016a2a654a79f0033503787e55afab
Andrzej Ressel <jereksel@gmail.com>
2017-08-25 09:52:42 +0200

    Update instrumentaltests.sh

--- instrumentaltests.sh
+++ instrumentaltests.sh
@@ -3,5 +3,7 @@
 emulator -avd circleci-android24 -no-window &
 circle-android wait-for-boot
 ./preparefortests.sh
-adb shell input keyevent 82
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $CIRCLE_ARTIFACTS/screen-$(date +"%T").png
+adb shell input keyevent 82 &
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $CIRCLE_ARTIFACTS/screen-$(date +"%T").png
 ./gradlew connectedAndroidTest
