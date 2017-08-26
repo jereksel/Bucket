4691ce355330c922663a2cee42b9cf6480d13174
Andrzej Ressel <jereksel@gmail.com>
2017-08-26 12:00:12 +0200

    Update

--- instrumentaltests.sh
+++ instrumentaltests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-emulator -avd circleci-android22 -no-window &
+emulator -avd circleci-android24-googleapis -no-window -gpu swiftshader &
+./gradlew -PdisablePreDex assembleAndroidTest
 circle-android wait-for-boot
 ./preparefortests.sh
 adb shell input keyevent 82
