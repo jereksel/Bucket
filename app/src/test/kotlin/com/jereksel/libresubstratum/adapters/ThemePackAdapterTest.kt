43a0ccf3d391270dfa93fcc78c05f667b9e2780d
Andrzej Ressel <jereksel@gmail.com>
2017-09-23 22:14:51 +0200

    Tests for ThemePackAdapter

--- app/src/test/kotlin/com/jereksel/libresubstratum/adapters/ThemePackAdapterTest.kt
+++ app/src/test/kotlin/com/jereksel/libresubstratum/adapters/ThemePackAdapterTest.kt
@@ -1,38 +1,40 @@
 package com.jereksel.libresubstratum.adapters
 
+//import org.assertj.core.api.Assertions.*
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.AdapterView
 import com.jereksel.libresubstratum.BuildConfig
 import com.jereksel.libresubstratum.MockedApp
 import com.jereksel.libresubstratum.RecViewActivity
 import com.jereksel.libresubstratum.ResettableLazy
-import com.jereksel.libresubstratum.activities.detailed.DetailedContract
 import com.jereksel.libresubstratum.activities.detailed.DetailedContract.Presenter
-import com.jereksel.libresubstratum.domain.IActivityProxy
-import io.kotlintest.mock.mock
-import kotlinx.android.synthetic.main.activity_reconly.*
-import org.assertj.android.api.Assertions.assertThat
-import org.assertj.android.recyclerview.v7.api.Assertions.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-import org.robolectric.android.controller.ActivityController
-import org.robolectric.annotation.Config
-import com.jereksel.libresubstratum.extensions.*
-import com.jereksel.libresubstratumlib.*
+import com.jereksel.libresubstratum.adapters.ThemePackAdapter.ViewHolder
+import com.jereksel.libresubstratum.data.Type1ExtensionToString
+import com.jereksel.libresubstratum.data.Type2ExtensionToString
+import com.jereksel.libresubstratum.extensions.getAllStrings
+import com.jereksel.libresubstratumlib.Type1Extension
+import com.jereksel.libresubstratumlib.Type2Extension
 import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.reset
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
-//import org.assertj.core.api.Assertions.*
+import kotlinx.android.synthetic.main.activity_reconly.*
+import org.assertj.android.recyclerview.v7.api.Assertions.assertThat
+import org.assertj.android.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.*
+import org.junit.Assert.*
+import org.junit.*
+import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class,
@@ -40,8 +42,8 @@ import org.mockito.MockitoAnnotations
         sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP))
 class ThemePackAdapterTest {
 
-    lateinit var activityController : ActivityController<RecViewActivity>
-    lateinit var activity : AppCompatActivity
+    lateinit var activityController: ActivityController<RecViewActivity>
+    lateinit var activity: AppCompatActivity
 
     var activityCasted by ResettableLazy { activity as AppCompatActivity? }
     var recyclerView by ResettableLazy { activityCasted!!.recyclerView }
@@ -76,7 +78,7 @@ class ThemePackAdapterTest {
         }
         assertThat(adapter_).hasItemCount(1)
         val child = recyclerView.layoutManager.findViewByPosition(0)
-        val viewHolder = recyclerView.getChildViewHolder(child) as ThemePackAdapter.ViewHolder
+        val viewHolder = recyclerView.getChildViewHolder(child) as ViewHolder
 
         viewHolder.checkbox.performClick()
         verify(presenter).setCheckbox(0, true)
@@ -102,18 +104,18 @@ class ThemePackAdapterTest {
         }
         assertThat(adapter_).hasItemCount(1)
         val child = recyclerView.layoutManager.findViewByPosition(0)
-        val viewHolder = recyclerView.getChildViewHolder(child) as ThemePackAdapter.ViewHolder
+        val viewHolder = recyclerView.getChildViewHolder(child) as ViewHolder
 
         viewHolder.card.performClick()
         verify(presenter).setCheckbox(0, true)
         verify(presenter, never()).setCheckbox(0, false)
     }
 
-/*
-    @Test
-    fun `Theme without extensions shouldn't have spinners`() {
-        val theme = ThemePack(listOf(Theme("app1")))
-        val adapter_ = ThemePackAdapter(theme, mock())
+//    @Ignore
+    @Ignore
+    fun getViewHolder(): ViewHolder {
+        whenever(presenter.getNumberOfThemes()).thenReturn(1)
+        val adapter_ = ThemePackAdapter(presenter)
         recyclerView.run {
             layoutManager = LinearLayoutManager(context)
             itemAnimator = DefaultItemAnimator()
@@ -123,51 +125,123 @@ class ThemePackAdapterTest {
         }
         assertThat(adapter_).hasItemCount(1)
         val child = recyclerView.layoutManager.findViewByPosition(0)
-        val viewHolder = recyclerView.getChildViewHolder(child) as ThemePackAdapter.ViewHolder
-        viewHolder.type1Spinners.forEach { assertThat(it).isGone }
-        assertThat(viewHolder.type2Spinner).isGone
+        return recyclerView.getChildViewHolder(child) as ViewHolder
     }
 
     @Test
-    fun `Theme with only type2 extension have t2 spinner visible and t1 spinners gone`() {
-        val type2s = listOf("a", "b", "c")
-        val theme = ThemePack(listOf(Theme("app1", type2 = Type2Data(type2s.map { Type2Extension(it, false) }))))
-        val adapter_ = ThemePackAdapter(theme, mock())
-        recyclerView.run {
-            layoutManager = LinearLayoutManager(context)
-            itemAnimator = DefaultItemAnimator()
-            adapter = adapter_
-            measure(0, 0)
-            layout(0, 0, 100, 10000)
-        }
-        assertThat(adapter_).hasItemCount(1)
-        val child = recyclerView.layoutManager.findViewByPosition(0)
-        val viewHolder = recyclerView.getChildViewHolder(child) as ThemePackAdapter.ViewHolder
-        viewHolder.type1Spinners.forEach { assertThat(it).isGone }
-        assertThat(viewHolder.type2Spinner).isVisible
-        assertThat(viewHolder.type2Spinner.getAllStrings()).isEqualTo(type2s)
+    fun `setAppId sets appId text`() {
+        val viewHolder = getViewHolder()
+        viewHolder.setAppId("themeid")
+        assertThat(viewHolder.appId).hasText("themeid")
+        viewHolder.setAppId("themeid2")
+        assertThat(viewHolder.appId).hasText("themeid2")
     }
 
     @Test
-    fun `Theme with only t1 extension should have t1 spinners visible and t2 spinner gone`() {
-        val type1s = listOf("a", "b", "c")
-        val theme = ThemePack(listOf(Theme("app1", type1 = listOf(Type1Data(type1s.map { Type1Extension(it, false) }, "a")))))
-        val adapter_ = ThemePackAdapter(theme, mock())
-        recyclerView.run {
-            layoutManager = LinearLayoutManager(context)
-            itemAnimator = DefaultItemAnimator()
-            adapter = adapter_
-            measure(0, 0)
-            layout(0, 0, 100, 10000)
+    fun `setAppName sets app text`() {
+        val viewHolder = getViewHolder()
+        viewHolder.setAppName("theme")
+        assertThat(viewHolder.appName).hasText("theme")
+        viewHolder.setAppName("theme 2")
+        assertThat(viewHolder.appName).hasText("theme 2")
+    }
+
+    @Test
+    fun `setCheckbox sets checkbox state`() {
+        val viewHolder = getViewHolder()
+        viewHolder.setCheckbox(true)
+        assertThat(viewHolder.checkbox).isChecked
+        viewHolder.setCheckbox(false)
+        assertThat(viewHolder.checkbox).isNotChecked
+    }
+
+    @Test
+    fun `Passing empty list to typeXSpinner should make is gone`() {
+        val viewHolder = getViewHolder()
+
+        val settera = { l: List<Type1ExtensionToString> -> viewHolder.type1aSpinner(l, 0) }
+        val setterb = { l: List<Type1ExtensionToString> -> viewHolder.type1bSpinner(l, 0) }
+        val setterc = { l: List<Type1ExtensionToString> -> viewHolder.type1cSpinner(l, 0) }
+
+        listOf(
+                viewHolder.type1aSpinner to settera,
+                viewHolder.type1bSpinner to setterb,
+                viewHolder.type1cSpinner to setterc
+        ).forEach { (holder, setter) ->
+            setter.invoke(listOf())
+            assertThat(holder).isGone
         }
-        assertThat(adapter_).hasItemCount(1)
-        val child = recyclerView.layoutManager.findViewByPosition(0)
-        val viewHolder = recyclerView.getChildViewHolder(child) as ThemePackAdapter.ViewHolder
-//        viewHolder.type1Spinners.forEach { assertThat(it).isGone }
-        assertThat(viewHolder.type1aSpinner).isVisible
-        assertThat(viewHolder.type1bSpinner).isGone
-        assertThat(viewHolder.type1cSpinner).isGone
+    }
+
+
+    @Test
+    fun `Passing list to type1XSpinner sets it as spinner's data`() {
+
+        val viewHolder = getViewHolder()
+
+        val settera = { l: List<Type1ExtensionToString>,i : Int -> viewHolder.type1aSpinner(l, i) }
+        val setterb = { l: List<Type1ExtensionToString>,i: Int -> viewHolder.type1bSpinner(l, i) }
+        val setterc = { l: List<Type1ExtensionToString>,i: Int -> viewHolder.type1cSpinner(l, i) }
+
+
+        listOf(
+                viewHolder.type1aSpinner to settera,
+                viewHolder.type1bSpinner to setterb,
+                viewHolder.type1cSpinner to setterc
+        ).forEach { (holder, setter) ->
+
+            val arr = listOf(
+                    Type1ExtensionToString(Type1Extension("red", true)),
+                    Type1ExtensionToString(Type1Extension("green", false))
+            )
+
+            holder.onItemSelectedListener= object: AdapterView.OnItemSelectedListener {
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+
+            setter.invoke(arr, 0)
+            assertThat(holder.adapter.getAllStrings()).hasSameElementsAs(listOf("red", "green"))
+            assertEquals(0, holder.selectedItemId)
+
+            setter.invoke(arr, 1)
+            assertThat(holder.adapter.getAllStrings()).hasSameElementsAs(listOf("red", "green"))
+            assertEquals(1, holder.selectedItemId)
+
+        }
+    }
+
+    @Test
+    fun `Passing empty list to type2Spinner hides type2 spinner`() {
+        val viewHolder = getViewHolder()
+        viewHolder.type2Spinner(listOf(), 0)
         assertThat(viewHolder.type2Spinner).isGone
-        assertThat(viewHolder.type1aSpinner.getAllStrings()).isEqualTo(type1s)
-    }*/
+    }
+
+
+    fun `Passing list to type2Spinner sets it as spinner's data`() {
+
+        val viewHolder = getViewHolder()
+        val list = listOf(
+                Type2ExtensionToString(Type2Extension("blue", true)),
+                Type2ExtensionToString(Type2Extension("green", true))
+        )
+
+        val holder = viewHolder.type2Spinner
+
+        viewHolder.type2Spinner(list, 0)
+        assertThat(holder.adapter.getAllStrings()).hasSameElementsAs(listOf("blue", "green"))
+        assertEquals(0, holder.selectedItemId)
+
+        viewHolder.type2Spinner(list, 1)
+        assertThat(holder.adapter.getAllStrings()).hasSameElementsAs(listOf("blue", "green"))
+        assertEquals(1, holder.selectedItemId)
+
+    }
+
+
 }
