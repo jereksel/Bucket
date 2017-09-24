086b2973eccde3d6b31793b26f5f69acc87f865d
Andrzej Ressel <jereksel@gmail.com>
2017-09-25 01:00:55 +0200

    Update

--- unittests.sh
+++ unittests.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-./gradlew testDebugUnitTest --no-daemon
+./gradlew testDebugUnitTest --no-daemon --tests \*DetailedPresenterTest
+./gradlew testDebugUnitTest --no-daemon --tests \*InstalledPresenterTest
+./gradlew testDebugUnitTest --no-daemon --tests \*MainPresenterTest
+
+./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.presenters.*
+
+./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.adapters.*
+./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.utils.*
+
+
 ./gradlew sublib:reader:test --no-daemon
 ./gradlew sublib:compiler:test --no-daemon
 
