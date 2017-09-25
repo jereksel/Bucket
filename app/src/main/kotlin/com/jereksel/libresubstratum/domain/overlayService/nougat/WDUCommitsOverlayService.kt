4389b5a01789914a6d56651deec473e5e2748e83
Andrzej Ressel <jereksel@gmail.com>
2017-09-25 19:38:28 +0200

    Ask for external storage permission

--- app/src/main/kotlin/com/jereksel/libresubstratum/domain/overlayService/nougat/WDUCommitsOverlayService.kt
+++ app/src/main/kotlin/com/jereksel/libresubstratum/domain/overlayService/nougat/WDUCommitsOverlayService.kt
@@ -1,11 +1,11 @@
 package com.jereksel.libresubstratum.domain.overlayService.nougat
 
-import android.Manifest
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Context
 import android.provider.Settings
 
 class WDUCommitsOverlayService(context: Context): InterfacerOverlayService(context) {
-    override fun allPermissions() = listOf("projekt.interfacer.permission.ACCESS_SERVICE_INNER")
+    override fun allPermissions() = listOf("projekt.interfacer.permission.ACCESS_SERVICE_INNER", WRITE_EXTERNAL_STORAGE)
 
     override fun additionalSteps(): String? {
 
