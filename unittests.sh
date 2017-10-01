f6f6b0784deb63feef8b7a0a044d147832f76b5b
Andrzej Ressel <jereksel@gmail.com>
2017-10-01 15:19:51 +0200

    Update

--- unittests.sh
+++ unittests.sh
@@ -2,14 +2,20 @@
 
 set -e
 
+# Robolectric
+
 ./gradlew testDebugUnitTest --no-daemon --tests \*MainViewTest
 ./gradlew testDebugUnitTest --no-daemon --tests \*DetailedViewTest
 ./gradlew testDebugUnitTest --no-daemon --tests \*InstalledViewTest
 
+./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.adapters.*
+
+# Non-Robolectric
+
 ./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.presenters.*
 
-./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.adapters.*
 ./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.utils.*
+./gradlew testDebugUnitTest --no-daemon --tests com.jereksel.libresubstratum.domain.usecases.*
 
 ./gradlew sublib:reader:test --no-daemon
 ./gradlew sublib:compiler:test --no-daemon
