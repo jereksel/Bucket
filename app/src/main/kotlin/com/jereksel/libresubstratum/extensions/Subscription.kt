00ac2f572dbfee658e79de172c6550443838e9fb
Andrzej Ressel <jereksel@gmail.com>
2017-10-03 23:16:16 +0200

    RxJava

--- app/src/main/kotlin/com/jereksel/libresubstratum/extensions/Subscription.kt
+++
@@ -1,5 +0,0 @@
-package com.jereksel.libresubstratum.extensions
-
-import rx.Subscription
-
-fun Subscription.safeUnsubscribe() = if (!isUnsubscribed) { unsubscribe() } else {}
---
+++ app/src/test/kotlin/com/jereksel/libresubstratum/presenters/PresenterTestUtils.kt
@@ -0,0 +1,7 @@
+package com.jereksel.libresubstratum.presenters
+
+/**
+ * Created by andrzej on 03.10.17.
+ */
+object PresenterTestUtils {
+}
