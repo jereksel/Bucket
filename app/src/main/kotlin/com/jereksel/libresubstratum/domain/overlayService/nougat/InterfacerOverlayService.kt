3fb8a0ca6275313555b70bcfd2046307adc1a7d1
Andrzej Ressel <jereksel@gmail.com>
2017-10-05 00:48:27 +0200

    Make OverlayService asynchronous(ish)
    In any case it fixes Interfacer's FC when Substratum is not installed

--- app/src/main/kotlin/com/jereksel/libresubstratum/domain/overlayService/nougat/InterfacerOverlayService.kt
+++ app/src/main/kotlin/com/jereksel/libresubstratum/domain/overlayService/nougat/InterfacerOverlayService.kt
@@ -4,24 +4,42 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.om.IOverlayManager
 import android.content.pm.PackageManager
 import android.os.IBinder
+import android.os.Looper
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import com.google.common.cache.*
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFutureTask
+import com.google.common.util.concurrent.Service
 import com.jereksel.libresubstratum.domain.OverlayInfo
 import com.jereksel.libresubstratum.domain.OverlayService
 import com.jereksel.libresubstratum.extensions.getLogger
 import com.jereksel.omslib.OMSLib
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import projekt.substratum.IInterfacerInterface
 import java.io.File
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
 abstract class InterfacerOverlayService(val context: Context): OverlayService {
 
     private val log = getLogger()
 
-    val oms = OMSLib.getOMS()!!
+    private val oms: IOverlayManager
+        get() = omsCache.get(Unit)
 
-    private lateinit var service: IInterfacerInterface
+    private val service: IInterfacerInterface
+        get() = interfacerCache.get(Unit).second
+
+    @Suppress("JoinDeclarationAndAssignment")
+    private val omsCache : LoadingCache<Unit, IOverlayManager>
+
+    private val interfacerCache : LoadingCache<Unit, Pair<ServiceConnection, IInterfacerInterface>>
 
     val INTERFACER_PACKAGE = "projekt.interfacer"
     val INTERFACER_SERVICE = INTERFACER_PACKAGE + ".services.JobService"
@@ -29,21 +47,53 @@ abstract class InterfacerOverlayService(val context: Context): OverlayService {
     val STATUS_CHANGED = INTERFACER_PACKAGE + ".STATUS_CHANGED"
 
     init {
-        val serviceConnection = object: ServiceConnection {
 
-            override fun onServiceConnected(name: ComponentName, binder: IBinder) {
-                log.debug("Interfacer service connected")
-                service = IInterfacerInterface.Stub.asInterface(binder)
-            }
+        omsCache = CacheBuilder.newBuilder()
+                .expireAfterAccess(1, TimeUnit.MINUTES)
+                .build(object: CacheLoader<Unit, IOverlayManager>() {
+                    override fun load(key: Unit) = OMSLib.getOMS()!!
+                })
 
-            override fun onServiceDisconnected(name: ComponentName) {
-            }
-        }
 
-        val intent = Intent(INTERFACER_BINDED)
-        intent.`package` = INTERFACER_PACKAGE
-        // binding to remote service
-        context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        interfacerCache = CacheBuilder.newBuilder()
+                .expireAfterAccess(1, TimeUnit.MINUTES)
+                .removalListener(RemovalListener<Unit, Pair<ServiceConnection, IInterfacerInterface>> {
+                    notification -> context.unbindService(notification.value.first)
+                })
+                .build(object: CacheLoader<Unit, Pair<ServiceConnection, IInterfacerInterface>>() {
+                    override fun load(key: Unit): Pair<ServiceConnection, IInterfacerInterface> {
+
+                        if (Looper.getMainLooper().thread == Thread.currentThread()) {
+                            throw RuntimeException("Cannot be invoked on UI thread")
+                        }
+
+                        return Observable.fromPublisher<Pair<ServiceConnection, IInterfacerInterface>> {
+
+                            val serviceConnection = object: ServiceConnection {
+
+                                override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+                                    log.debug("Interfacer service connected")
+                                    val service = IInterfacerInterface.Stub.asInterface(binder)
+                                    it.onNext(Pair(this, service))
+                                    it.onComplete()
+                                }
+
+                                override fun onServiceDisconnected(name: ComponentName) {
+                                    it.onError(RuntimeException("Service disconnected"))
+                                }
+                            }
+
+                            val intent = Intent(INTERFACER_BINDED)
+                            intent.`package` = INTERFACER_PACKAGE
+                            // binding to remote service
+                            context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+                        }
+                                .subscribeOn(AndroidSchedulers.mainThread())
+                                .observeOn(Schedulers.computation())
+                                .blockingFirst()
+                    }
+                })
+
 
     }
 
@@ -60,6 +110,7 @@ abstract class InterfacerOverlayService(val context: Context): OverlayService {
         return OverlayInfo(id, info.isEnabled)
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun getAllOverlaysForApk(appId: String): List<OverlayInfo> {
         val map = oms.getOverlayInfosForTarget(appId, 0) as List<android.content.om.OverlayInfo>
         return map.map { OverlayInfo(it.packageName, it.isEnabled) }
