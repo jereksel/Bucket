6421b09d19c1fb290d930718458e3d2c230b0be9
dimqua <dimqua@riseup.net>
2017-10-06 02:35:53 +0300

    add F-Droid flavor

---
+++ app/src/fdroid/java/com/jereksel/libresubstratum/App.java
@@ -0,0 +1,39 @@
+package com.jereksel.libresubstratum;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+
+import com.jereksel.libresubstratum.dagger.components.AppComponent;
+import com.jereksel.libresubstratum.dagger.components.DaggerAppComponent;
+import com.jereksel.libresubstratum.dagger.modules.AppModule;
+
+public class App extends Application {
+
+    private AppComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @VisibleForTesting
+    protected AppComponent createComponent() {
+        return DaggerAppComponent.builder()
+                .appModule(getAppModule())
+                .build();
+    }
+
+    protected AppModule getAppModule() {
+        return new AppModule(this);
+    }
+
+    public AppComponent getAppComponent(Context context) {
+        App app = (App) context.getApplicationContext();
+        if (app.component == null) {
+            app.component = app.createComponent();
+        }
+        return app.component;
+    }
+
+}
