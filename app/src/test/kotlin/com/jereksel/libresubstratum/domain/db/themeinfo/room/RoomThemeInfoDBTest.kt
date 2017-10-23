f0c52ca2e73c1b176c863c9d705220f35a80be7b
Andrzej Ressel <jereksel@gmail.com>
2017-10-23 17:53:14 +0200

    Test for Room

--- app/src/test/kotlin/com/jereksel/libresubstratum/domain/db/themeinfo/room/RoomThemeInfoDBTest.kt
+++ app/src/test/kotlin/com/jereksel/libresubstratum/domain/db/themeinfo/room/RoomThemeInfoDBTest.kt
@@ -2,8 +2,7 @@ package com.jereksel.libresubstratum.domain.db.themeinfo.room
 
 import com.jereksel.libresubstratum.BaseRobolectricTest
 import com.jereksel.libresubstratum.domain.ThemePackDatabase
-import com.jereksel.libresubstratumlib.Theme
-import com.jereksel.libresubstratumlib.ThemePack
+import com.jereksel.libresubstratumlib.*
 import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -22,6 +21,33 @@ class RoomThemeInfoDBTest: BaseRobolectricTest() {
     @Test
     fun `Basic theme pack test`() {
         val themePack = ThemePack(listOf(Theme("app1"), Theme("app2")))
+        db.addThemePack("asd", themePack)
+        assertEquals(themePack, db.getThemePack("asd"))
+    }
+
+    @Test
+    fun `Type1 theme pack test`() {
+        val type1a = Type1Data(listOf(Type1Extension("yellow", true), Type1Extension("blue", false)), "a")
+        val type1b = Type1Data(listOf(Type1Extension("red", true), Type1Extension("pink", false)), "b")
+        val type1c = Type1Data(listOf(Type1Extension("white", true), Type1Extension("black", false)), "c")
+        val themePack = ThemePack(listOf(Theme("app1", listOf(type1a, type1c)), Theme("app2", listOf(type1b)), Theme("app3")))
+        db.addThemePack("asd", themePack)
+        assertEquals(themePack, db.getThemePack("asd"))
+    }
+
+    @Test
+    fun `Type2 theme pack test`() {
+        val type2a = Type2Data(listOf(Type2Extension("yellow", true), Type2Extension("blue", false)))
+        val type2b = Type2Data(listOf(Type2Extension("red", true), Type2Extension("pink", false)))
+        val themePack = ThemePack(listOf(Theme("app1", type2 = type2a), Theme("app2", type2 = type2b), Theme("app3")))
+        db.addThemePack("asd", themePack)
+        assertEquals(themePack, db.getThemePack("asd"))
+    }
+
+    @Test
+    fun `Type3 theme pack test`() {
+        val type3 = Type3Data(listOf(Type3Extension("yellow", true), Type3Extension("blue", false)))
+        val themePack = ThemePack(listOf(Theme("app1"), Theme("app2"), Theme("app3")), type3)
         db.addThemePack("asd", themePack)
         assertEquals(themePack, db.getThemePack("asd"))
     }
