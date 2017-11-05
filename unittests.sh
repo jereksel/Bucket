863ec361f0719f203d05e102f85206e631d49d10
Andrzej Ressel <jereksel@gmail.com>
2017-11-05 19:26:47 +0100

    Update

--- unittests.sh
+++ unittests.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "**.MainViewTest"
+export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false"
+
+# To download Robolectric libs
+./gradlew app:testFdroidDebugUnitTest --tests "**.BaseRobolectricTest"
 
 set -e
 
 # Robolectric
 
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "**.MainViewTest"
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "**.DetailedViewTest"
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "**.InstalledViewTest"
+./gradlew app:testFdroidDebugUnitTest --tests "**.MainViewTest"
+./gradlew app:testFdroidDebugUnitTest --tests "**.DetailedViewTest"
+./gradlew app:testFdroidDebugUnitTest --tests "**.InstalledViewTest"
 
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "com.jereksel.libresubstratum.adapters.*"
+./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.adapters.*"
 
 # Non-Robolectric
 
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "com.jereksel.libresubstratum.presenters.*"
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "com.jereksel.libresubstratum.data.*"
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "com.jereksel.libresubstratum.utils.*"
-./gradlew app:testFdroidDebugUnitTest --no-daemon --tests "com.jereksel.libresubstratum.domain.*"
+./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.presenters.*"
+./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.data.*"
+./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.utils.*"
+./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.domain.*"
 
-./gradlew sublib:reader:test --no-daemon
-./gradlew sublib:compiler:test --no-daemon
-./gradlew sublib:themereaderassetmanager:test --no-daemon
-./gradlew sublib:compilerassetmanager:test --no-daemon
+./gradlew sublib:reader:test
+./gradlew sublib:compiler:test
+./gradlew sublib:themereaderassetmanager:test
+./gradlew sublib:compilerassetmanager:test
 
-./gradlew app:jacocoTestReport --no-daemon
-./gradlew sublib:reader:jacocoTestReport --no-daemon
-./gradlew sublib:compiler:jacocoTestReport --no-daemon
-./gradlew sublib:themereaderassetmanager:jacocoTestReportDebug --no-daemon
-./gradlew sublib:compilerassetmanager:jacocoTestReportDebug --no-daemon
+./gradlew app:jacocoTestReport
+./gradlew sublib:reader:jacocoTestReport
+./gradlew sublib:compiler:jacocoTestReport
+./gradlew sublib:themereaderassetmanager:jacocoTestReportDebug
+./gradlew sublib:compilerassetmanager:jacocoTestReportDebug
