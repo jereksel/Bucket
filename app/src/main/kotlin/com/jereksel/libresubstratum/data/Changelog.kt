6951b618ab2a080381771ac8c41687de39016913
Andrzej Ressel <jereksel@gmail.com>
2017-11-25 20:49:40 +0100

    Fix InstalledView FC

--- app/src/main/kotlin/com/jereksel/libresubstratum/data/Changelog.kt
+++ app/src/main/kotlin/com/jereksel/libresubstratum/data/Changelog.kt
@@ -5,6 +5,7 @@ import com.jereksel.changelogdialog.changelog
 object Changelog {
     val changelog = changelog {
         betaVersion("0.2-BETA") {
+            +"Fix FC in installed overlays view"
             +"Searching in installed overlays view"
             +"Change toast text that shows when theme cannot be decrypted"
             +"Add screen about crash after FC"
