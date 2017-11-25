7071ed57f65d6b93bfd96da67ccbc515691aa311
Andrzej Ressel <jereksel@gmail.com>
2017-11-25 21:36:48 +0100

    Update

--- app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
+++ app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
@@ -17,17 +17,25 @@ class CrashlitycsMetrics
     override fun userEnteredTheme(themeId: String) {
         data["currentTheme"] = themeId
         Crashlytics.setString("currentTheme", themeId)
+        log.debug("User entered theme {}", themeId)
     }
 
-    override fun userCompiledOverlay(themeId: String, targetApp: String) = Unit
+    override fun userCompiledOverlay(themeId: String, targetApp: String) {
+        log.debug("User compiled overlay from theme {} for app {}", themeId, targetApp)
+    }
 
-    override fun userEnabledOverlay(overlayId: String) = Unit
+    override fun userEnabledOverlay(overlayId: String) {
+        log.debug("User enabled overlay {}", overlayId)
+    }
 
-    override fun userDisabledOverlay(overlayId: String) = Unit
+    override fun userDisabledOverlay(overlayId: String) {
+        log.debug("User disabled overlay {}", overlayId)
+    }
 
     override fun logOverlayServiceType(overlayService: OverlayService) {
         data["overlayService"] = overlayService.javaClass.toString()
         Crashlytics.setString("overlayService", overlayService.javaClass.toString())
+        log.debug("User overlayService: {}", overlayService.javaClass.toString())
     }
 
     override fun getMetrics() = data
