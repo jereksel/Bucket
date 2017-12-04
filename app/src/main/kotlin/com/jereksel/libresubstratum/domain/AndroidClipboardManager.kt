0f12f1b1799d547a365d398ac0faf48e6ec04ec6
Andrzej Ressel <jereksel@gmail.com>
2017-12-04 18:21:43 +0100

    Change app name to "Bucket"

--- app/src/main/kotlin/com/jereksel/libresubstratum/domain/AndroidClipboardManager.kt
+++ app/src/main/kotlin/com/jereksel/libresubstratum/domain/AndroidClipboardManager.kt
@@ -8,7 +8,7 @@ typealias RealClipboardManager = android.content.ClipboardManager
 class AndroidClipboardManager(private val context: Context): ClipboardManager {
     override fun addToClipboard(message: String) {
         val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as RealClipboardManager
-        val clip = ClipData.newPlainText("LibreSubstratum", message)
+        val clip = ClipData.newPlainText("BucketThemeManager", message)
         clipboard.primaryClip = clip
     }
 }
