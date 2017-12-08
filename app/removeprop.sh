acdb80b399c9519055edee7023237690786c38a6
Andrzej Ressel <jereksel@gmail.com>
2017-12-08 01:00:43 +0100

    Fix fabric removal script

--- app/removeprop.sh
+++ app/removeprop.sh
@@ -2,6 +2,6 @@
 
 sed -i "s|maven { url 'https://maven.fabric.io/public' }||g" build.gradle
 
-sed -i "s|classpath 'io.fabric.tools:gradle:1.24.1'||g" build.gradle
+sed -i "s|classpath 'io.fabric.tools:gradle:1.24.4'||g" build.gradle
 
 sed -i "s|apply plugin: 'io.fabric'||g" build.gradle
