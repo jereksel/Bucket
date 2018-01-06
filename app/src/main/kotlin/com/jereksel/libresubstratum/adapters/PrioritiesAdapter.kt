1f46d93311e185b03335fe407ec433da6f57e6a2
Andrzej Ressel <jereksel@gmail.com>
2018-01-06 19:00:52 +0100

    Update

--- app/src/main/kotlin/com/jereksel/libresubstratum/adapters/PrioritiesAdapter.kt
+++ app/src/main/kotlin/com/jereksel/libresubstratum/adapters/PrioritiesAdapter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Andrzej Ressel (jereksel@gmail.com)
+ * Copyright (C) 2017 Andrzej Ressel (jereksel@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
