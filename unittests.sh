faf51bcfc4c7d5a31cfe04526fee08ddf81b9b76
Andrzej Ressel <jereksel@gmail.com>
2018-01-19 21:25:21 +0100

    Update

--- unittests.sh
+++ unittests.sh
@@ -7,23 +7,7 @@ export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false"
 
 set -e
 
-# Robolectric
-
-./gradlew app:testFdroidDebugUnitTest --tests "**.MainViewTest"
-./gradlew app:testFdroidDebugUnitTest --tests "**.DetailedViewTest"
-./gradlew app:testFdroidDebugUnitTest --tests "**.InstalledViewTest"
-./gradlew app:testFdroidDebugUnitTest --tests "**.PrioritiesViewTest"
-./gradlew app:testFdroidDebugUnitTest --tests "**.PrioritiesDetailViewTest"
-
-./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.adapters.*"
-
-# Non-Robolectric
-
-./gradlew app:testFdroidDebugUnitTest --tests "**.MainViewViewModelTest"
-./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.presenters.*"
-./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.data.*"
-./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.utils.*"
-./gradlew app:testFdroidDebugUnitTest --tests "com.jereksel.libresubstratum.domain.*"
+./gradlew app:testFdroidDebugUnitTest
 
 ./gradlew sublib:reader:test
 ./gradlew sublib:compiler:test
