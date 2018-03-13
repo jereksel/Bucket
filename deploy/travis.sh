c70b6c5e3bfa04f80d8b741eb325ca72125573bf
Andrzej Ressel <jereksel@gmail.com>
2018-03-13 22:26:11 +0100

    Update

--- deploy/travis.sh
+++ deploy/travis.sh
@@ -3,4 +3,9 @@
 export BUILD_NUMBER=$(($TRAVIS_BUILD_NUMBER + 1125))
 export BRANCH=$TRAVIS_BRANCH
 
-./gradlew publishPlayRelease
+cd app
+openssl aes-256-cbc -K $KEY -iv $IV -in secrets.tar.enc -out secrets.tar -d
+tar xvf secrets.tar
+cd ..
+./gradlew publishPlayRelease --no-daemon
+
