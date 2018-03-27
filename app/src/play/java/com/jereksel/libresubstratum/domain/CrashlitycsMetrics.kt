9e0c6096208137ac19a0ea811558c81e8f4171e1
Andrzej Ressel <jereksel@gmail.com>
2018-03-27 21:34:07 +0200

    Restore Answers

--- app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
+++ app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
@@ -1,6 +1,8 @@
 package com.jereksel.libresubstratum.domain
 
 import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import com.crashlytics.android.answers.CustomEvent
 import com.jereksel.libresubstratum.extensions.getLogger
 import javax.inject.Inject
 
@@ -18,18 +20,27 @@ class CrashlitycsMetrics
         data["currentTheme"] = themeId
         Crashlytics.setString("currentTheme", themeId)
         log.debug("User entered theme {}", themeId)
+        Answers.getInstance().logCustom(CustomEvent("Entered theme")
+                .putCustomAttribute("themeId", themeId))
     }
 
     override fun userCompiledOverlay(themeId: String, targetApp: String) {
         log.debug("User compiled overlay from theme {} for app {}", themeId, targetApp)
+        Answers.getInstance().logCustom(CustomEvent("Compiled overlay")
+                .putCustomAttribute("themeId", themeId)
+                .putCustomAttribute("targetApp", targetApp))
     }
 
     override fun userEnabledOverlay(overlayId: String) {
         log.debug("User enabled overlay {}", overlayId)
+        Answers.getInstance().logCustom(CustomEvent("Enabled overlay")
+                .putCustomAttribute("overlayId", overlayId))
     }
 
     override fun userDisabledOverlay(overlayId: String) {
         log.debug("User disabled overlay {}", overlayId)
+        Answers.getInstance().logCustom(CustomEvent("Disabled overlay")
+                .putCustomAttribute("overlayId", overlayId))
     }
 
     override fun logOverlayServiceType(overlayService: OverlayService) {
