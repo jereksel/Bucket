a4cb7de7d64f6e13ac8f18023c84483435b7fb41
Andrzej Ressel <jereksel@gmail.com>
2018-04-15 23:04:06 +0200

    Update ChangeLog.kt

--- libs/changelogdialog/src/main/java/com/jereksel/changelogdialog/ChangeLog.kt
+++ libs/changelogdialog/src/main/java/com/jereksel/changelogdialog/ChangeLog.kt
@@ -4,7 +4,7 @@ data class ChangeLog(
         val versions: MutableList<ChangeLogVersion> = mutableListOf()
 ) {
     fun version(name: String, init: ChangeLogVersion.() -> Unit) {
-        val version = ChangeLogVersion( name)
+        val version = ChangeLogVersion(name)
         version.init()
         versions += version
     }
