df4b1cc6b906e88ddba45ad6c132715084e0ec34
Andrzej Ressel <jereksel@gmail.com>
2018-05-12 14:18:20 +0200

    Remove Crashlytics (#266)

--- app/removeprop.sh
+++ app/removeprop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s|maven { url 'https://maven.fabric.io/public' }||g" build.gradle
+#sed -i "s|maven { url 'https://maven.fabric.io/public' }||g" build.gradle
 
-sed -i "s|classpath 'io.fabric.tools:gradle:1.25.2'||g" build.gradle
+#sed -i "s|classpath 'io.fabric.tools:gradle:1.25.2'||g" build.gradle
 
-sed -i "s|apply plugin: 'io.fabric'||g" build.gradle
+#sed -i "s|apply plugin: 'io.fabric'||g" build.gradle
--- app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
+++ app/src/play/java/com/jereksel/libresubstratum/domain/CrashlitycsMetrics.kt
@@ -1,8 +1,5 @@
 package com.jereksel.libresubstratum.domain
 
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.answers.Answers
-import com.crashlytics.android.answers.CustomEvent
 import com.jereksel.libresubstratum.extensions.getLogger
 import javax.inject.Inject
 
@@ -18,34 +15,23 @@ class CrashlitycsMetrics
 
     override fun userEnteredTheme(themeId: String) {
         data["currentTheme"] = themeId
-        Crashlytics.setString("currentTheme", themeId)
         log.debug("User entered theme {}", themeId)
-        Answers.getInstance().logCustom(CustomEvent("Entered theme")
-                .putCustomAttribute("themeId", themeId))
     }
 
     override fun userCompiledOverlay(themeId: String, targetApp: String) {
         log.debug("User compiled overlay from theme {} for app {}", themeId, targetApp)
-        Answers.getInstance().logCustom(CustomEvent("Compiled overlay")
-                .putCustomAttribute("themeId", themeId)
-                .putCustomAttribute("targetApp", targetApp))
     }
 
     override fun userEnabledOverlay(overlayId: String) {
         log.debug("User enabled overlay {}", overlayId)
-        Answers.getInstance().logCustom(CustomEvent("Enabled overlay")
-                .putCustomAttribute("overlayId", overlayId))
     }
 
     override fun userDisabledOverlay(overlayId: String) {
         log.debug("User disabled overlay {}", overlayId)
-        Answers.getInstance().logCustom(CustomEvent("Disabled overlay")
-                .putCustomAttribute("overlayId", overlayId))
     }
 
     override fun logOverlayServiceType(overlayService: OverlayService) {
         data["overlayService"] = overlayService.javaClass.toString()
-        Crashlytics.setString("overlayService", overlayService.javaClass.toString())
         log.debug("User overlayService: {}", overlayService.javaClass.toString())
     }
 
